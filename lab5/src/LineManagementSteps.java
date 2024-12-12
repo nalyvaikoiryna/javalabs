6ae021af628f82cbb358804e2154ea82078e4f25
WIN-8V3EAEGGP7H\user <nalyvaikoirynaolimpzl10@gmail.com>
2024-12-12 18:56:52 +0200

    тести для сценарію запуску виробничої лінії

---
+++ lab5/src/LineManagementSteps.java
@@ -0,0 +1,34 @@
+public class LineManagementSteps {
+
+    private ProductionFacade facade;
+
+    @Given("виробнича лінія зупинена")
+    public void productionLineStopped() {
+        facade = new ProductionFacade();
+        facade.stopLine();
+    }
+
+    @When("адміністратор надсилає команду {string}")
+    public void administratorSendsCommand(String command) {
+        if ("Запустити лінію".equals(command)) {
+            facade.startLine();
+        } else {
+            throw new IllegalArgumentException("Невідома команда: " + command);
+        }
+    }
+
+    @Then("конвеєр починає рух")
+    public void conveyorStarts() {
+        assert facade.isConveyorRunning() : "Конвеєр не працює!";
+    }
+
+    @Then("робот виконує свою операцію")
+    public void robotPerformsOperation() {
+        assert facade.isRobotOperating() : "Робот не виконує операцію!";
+    }
+
+    @Then("датчик сигналізує про завершення операції")
+    public void sensorSignalsCompletion() {
+        assert facade.isOperationComplete() : "Датчик не сигналізує завершення!";
+    }
+}
