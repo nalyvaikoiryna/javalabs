458745e7cf2a90bf86d23e32126ec4a28336b408
WIN-8V3EAEGGP7H\user <nalyvaikoirynaolimpzl10@gmail.com>
2024-12-20 01:17:08 +0200

    оновлено тести відповідно нового сценарію

--- project5/src/test/java/org/example/steps/LineManagementSteps.java
+++ project5/src/test/java/org/example/steps/LineManagementSteps.java
@@ -1,8 +1,10 @@
 package org.example.steps;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 import org.example.ProductionFacade;
+
 public class LineManagementSteps {
     private ProductionFacade facade;
 
@@ -12,10 +14,18 @@ public class LineManagementSteps {
         facade.stopLine();
     }
 
-    @When("адміністратор надсилає команду *string}")
+    @Given("виробнича лінія працює")
+    public void productionLineRunning() {
+        facade = new ProductionFacade();
+        facade.startLine();
+    }
+
+    @When("адміністратор надсилає команду {string}")
     public void administratorSendsCommand(String command) {
         if ("Запустити лінію".equals(command)) {
             facade.startLine();
+        } else if ("Зупинити лінію".equals(command)) {
+            facade.stopLine();
         } else {
             throw new IllegalArgumentException("Невідома команда: " + command);
         }
@@ -24,15 +34,35 @@ public class LineManagementSteps {
     @Then("конвеєр починає рух")
     public void conveyorStarts() {
         assert facade.isConveyorRunning() : "Конвеєр не працює!";
+        System.out.println("Конвеєр почав рух.");
     }
 
     @Then("робот виконує свою операцію")
     public void robotPerformsOperation() {
         assert facade.isRobotOperating() : "Робот не виконує операцію!";
+        System.out.println("Робот виконує свою операцію.");
     }
 
     @Then("датчик сигналізує про завершення операції")
     public void sensorSignalsCompletion() {
         assert facade.isOperationComplete() : "Датчик не сигналізує завершення!";
+        System.out.println("Датчик сигналізує про завершення операції.");
+    }
+
+    @Then("конвеєр зупиняється")
+    public void conveyorStops() {
+        assert !facade.isConveyorRunning() : "Конвеєр все ще працює!";
+        System.out.println("Конвеєр зупинився.");
+    }
+
+    @Then("робот припиняє операцію")
+    public void robotStopsOperation() {
+        assert !facade.isRobotOperating() : "Робот все ще працює!";
+        System.out.println("Робот припинив операцію.");
+    }
+
+    @Then("датчик деактивується")
+    public void sensorDeactivates() {
+        System.out.println("Датчик деактивовано.");
     }
 }
